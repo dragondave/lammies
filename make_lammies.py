6c3bcf9e16ff0fcfe3ee62f4945b23a19db05912
Dragon Dave McKee <dave.mckee@gmail.com>
2023-08-15 21:43:26 +0100

    looking good

--- make_lammies.py
+++ make_lammies.py
@@ -4,6 +4,29 @@ from jinja2 import Environment, FileSystemLoader
 templates_dir = "templates"
 env = Environment(loader=FileSystemLoader(templates_dir))
 lammy_template = env.get_template('one_lammy.html')
+page_template = env.get_template('page.html')
+
+class Page:
+    def __init__(self):
+        self.lammies = []
+
+    def render(self):
+        return page_template.render(lammies=self.lammies)
+
+    def from_csv(self, filename):
+        with open(filename, "r") as csvfile:
+            reader = csv.DictReader(csvfile) # TODO: replace
+            for row in reader:
+                for key, value in row.items():
+                    # this is a hack
+                    if value == "None":
+                        row[key] = None
+                if row['ref']:
+                    for i in range(0, int(row['printcount'])):
+                        self.lammies.append(Lammy(row))
+                    print(row)
+        return self
+
 
 
 class Lammy:
@@ -11,6 +34,7 @@ class Lammy:
         self.lammydict = {
             'ref': lammydict['ref'],
             'fronttext': lammydict['fronttext'],
+            'frontflavour': lammydict.get('frontflavour', None),
             'roleplaying': lammydict.get("roleplaying", None),
             'mechanical': lammydict.get("mechanical", None),
             'religious': lammydict.get("religious", None),
@@ -23,14 +47,6 @@ class Lammy:
         return lammy_template.render(**self.lammydict)
 
 
-
-def one_csv(filename):
-    with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            yield Lammy(row)
-
-
-marks = list(one_csv('data/marks.csv'))
-
-print (marks[0].render())
+page = Page().from_csv("data/bigsheet.csv")
+with open("output.html", "wb") as f:
+    f.write(page.render().encode('utf-8'))
